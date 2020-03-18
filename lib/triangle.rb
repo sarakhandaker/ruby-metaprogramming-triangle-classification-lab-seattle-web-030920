b4ba62cf9ee1f3103a9b4a2d1f632c1b84983700
github username <github email address>
2020-03-18 13:18:44 -0700

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,7 @@ class Triangle
   end
 
   def kind
-    if l1+l2 <l3 || l1<=0 || l2 <=0|| l3<=0|| l2+l3 <l1 ||l1+l3 <=l2
+    if l1+l2 <=l3 || l1<=0 || l2 <=0|| l3<=0|| l2+l3 <=l1 ||l1+l3 <=l2
       raise TriangleError
     elsif l1==l2 && l2==l3
       :equilateral
